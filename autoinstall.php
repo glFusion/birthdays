f5c683177de017261170d6d9b504a2b4dd7f88a7
Lee Garner <lee@leegarner.com>
2018-03-18 21:48:49 -0700

    Add auto-install file and public_html

---
+++ autoinstall.php
@@ -0,0 +1,117 @@
+<?php
+/**
+*   Provides automatic installation of the Birthdays plugin.
+*   There is nothing to do except create the plugin record
+*   since there are no tables or user interfaces.
+*
+*   @author     Lee Garner <lee@leegarner.com>
+*   @copyright  Copyright (c) 2018 Lee Garner <lee@leegarner.com>
+*   @package    birthdays
+*   @version    0.1.0
+*   @license    http://opensource.org/licenses/gpl-2.0.php
+*               GNU Public License v2 or later
+*   @filesource
+*/
+
+if (!defined ('GVERSION')) {
+    die ('This file can not be used on its own.');
+}
+
+/** @global string $_DB_dbms */
+global $_DB_dbms;
+
+require_once __DIR__ . '/functions.inc';
+require_once __DIR__ . '/birthdays.php';
+require_once __DIR__ . "/sql/{$_DB_dbms}_install.php";
+
+//  Plugin installation options
+$INSTALL_plugin[$_BD_CONF['pi_name']] = array(
+    'installer' => array('type' => 'installer',
+            'version' => '1',
+            'mode' => 'install',
+    ),
+
+    'plugin' => array('type' => 'plugin',
+            'name'      => $_BD_CONF['pi_name'],
+            'ver'       => $_BD_CONF['pi_version'],
+            'gl_ver'    => $_BD_CONF['gl_version'],
+            'url'       => $_BD_CONF['pi_url'],
+            'display'   => $_BD_CONF['pi_display_name']
+    ),
+    array('type' => 'table',
+            'table'     => $_TABLES['birthdays'],
+            'sql'       => $_SQL['birthdays'],
+    ),
+    array('type' => 'block', 
+            'name' => 'birthdays', 
+            'title' => $_BD_CONF['pi_display_name'],
+            'phpblockfn' => 'phpblock_birthdays', 
+            'block_type' => 'phpblock',
+            'group_id' => 'admin_group_id',
+    ),
+
+    array('type' => 'feature', 
+            'feature' => 'birthdays.view', 
+            'desc' => 'View birthdays',
+            'variable' => 'view_feature_id',
+    ),
+    array('type' => 'mapping',
+            'findgroup' => 'Logged-in Users',
+            'feature' => 'view_feature_id',
+            'log' => 'Adding birthdays.view feature to the logged-in users group',
+    ),
+
+);
+
+
+/**
+*   Puts the datastructures for this plugin into the glFusion database
+*   Note: Corresponding uninstall routine is in functions.inc
+*
+*   @return boolean     True if successful False otherwise
+*/
+function plugin_install_birthdays()
+{
+    global $INSTALL_plugin, $_BD_CONF;
+
+    COM_errorLog("Attempting to install the {$_BD_CONF['pi_name']} plugin", 1);
+    $ret = INSTALLER_install($INSTALL_plugin[$_BD_CONF['pi_name']]);
+    if ($ret > 0) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+/**
+*   Automatic removal function.
+*
+*   @return array       Array of items to be removed.
+*/
+function plugin_autouninstall_birthdays()
+{  
+    $out = array (
+        'tables'    => array('birthdays'),
+        'groups'    => array(),
+        'features'  => array('birthdays.view'),
+        'php_blocks' => array('birthdays'),
+        'vars'      => array(),
+    );
+    return $out;
+}
+
+
+/**
+*   Loads the configuration records for the Online Config Manager.
+*
+*   @return boolean     True = proceed, False = an error occured
+*/
+function plugin_load_configuration_birthdays()
+{
+COM_errorLog("setting up configurations");
+    require_once __DIR__ . '/install_defaults.php';
+    return plugin_initconfig_birthdays();
+}
+
+?>
---
+++ public_html/index.php
@@ -0,0 +1,150 @@
+<?php
+/**
+*   glFusion API functions for the Birthdays plugin.
+*
+*   @author     Lee Garner <lee@leegarner.com>
+*   @author     Mike Lynn <mike@mlynn.com>
+*   @copyright  Copyright (c) 2018 Lee Garner <lee@leegarner.com>
+*   @copyright  Copyright (c) 2002 Mike Lynn <mike@mlynn.com>
+*   @package    birthdays
+*   @version    0.1.0
+*   @license    http://opensource.org/licenses/gpl-2.0.php
+*               GNU Public License v2 or later
+*   @filesource
+*/
+require_once('../lib-common.php');
+USES_lib_admin();
+
+global $T, $_TABLES, $PHP_SELF, $_CONF, $HTTP_POST_VARS, $_USER, $LANG_STATIC,$_SP_CONF;
+
+/*
+* Main Function
+*/
+$mode = isset($_REQUEST['mode']) ? $_REQUEST['mode'] : '';
+$filter_month = isset($_REQUEST['filter_month']) ? $_REQUEST['filter_month'] : 0;
+
+$display = COM_siteHeader('menu');
+$T = new Template($_CONF['path'] . 'plugins/birthdays/templates');
+$T->set_file('header', 'index.thtml');
+$T->set_var(array(
+    'header'    =>$_BD_CONF['pi_display_name'],
+    'logo'      => plugin_geticon_birthdays(),
+) );
+$T->parse('output','header');
+$display .= $T->finish($T->get_var('output'));
+$display .= listbirthdays($filter_month);
+$display .= COM_siteFooter();
+echo $display;
+exit;
+
+
+/**
+*   Present the list of birthdays.
+*
+*   @param  integer $filter_month   Month to show, or "all"
+*   @return string      HTML for the list
+*/
+function listbirthdays($filter_month)
+{
+    global $T, $_TABLES, $PHP_SELF, $_CONF, $HTTP_POST_VARS, $_USER, $LANG_STATIC,$_SP_CONF;
+    global $LANG_BD00, $_BD_CONF, $LANG04, $LANG_ADMIN;
+
+    $retval = '';
+
+    $header_arr = array(
+        array('text' => $LANG04[3],
+            'field' => 'fullname',
+            'sort' => false,
+            'align' => '',
+        ),
+        array('text' => $LANG_BD00['birthday'],
+            'field' => 'birthday',
+            'sort' => true,
+            'align' => 'center',
+        ),
+    );
+
+    $dt = new \Date('now', $_CONF['timezone']);
+    $curmonth = $dt->Format('n', true);
+    $year = $dt->Format('Y', true);     // just to have a value for later
+    if (empty($filter_month)) {
+        $filter_month = $curmonth;
+    }
+    $filter = $filter_month == 'all' ? '' : " AND month = $filter_month";
+    $text_arr = array(
+        'form_url' => $_BD_CONF['url'] . '/index.php',
+    );
+    $query_arr = array('table' => 'birthdays',
+        'sql' => "SELECT * FROM {$_TABLES['birthdays']}
+                WHERE 1=1 $filter",
+        'query_fields' => array(),
+        'default_filter' => ''
+    );
+    $defsort_arr = array('field' => 'day', 'direction' => 'ASC');
+    $data_arr = Birthdays\Birthday::getAll($filter_month);
+    $form_arr = array(
+        'top' => select_month($filter_month),
+    );
+    $extra = array(
+        'dt'    => $dt,
+    );
+    $retval .= ADMIN_listArray('birthdays', 'getField_bday_list', $header_arr,
+                $text_arr, $data_arr, $defsort_arr, '', $extra, '', $form_arr);
+    return $retval;
+}
+
+
+/**
+*   Determine what to display in the admin list for each birthday.
+*
+*   @param  string  $fieldname  Name of the field, from database
+*   @param  mixed   $fieldvalue Value of the current field
+*   @param  array   $A          Array of all name/field pairs
+*   @param  array   $icon_arr   Array of system icons
+*   @param  array   $extra      Extra passthrough items (includes date obj)
+*   @return string              HTML for the field cell
+*/
+function getField_bday_list($fieldname, $fieldvalue, $A, $icon_arr, $extra)
+{
+    global $_CONF, $_BD_CONF;
+
+    $retval = '';
+
+    switch($fieldname) {
+    case 'fullname':
+        $retval .= COM_getDisplayName($A['uid']);
+        break;
+
+    case 'birthday':
+        $extra['dt']->setDate(2008, $A['month'], $A['day']);
+        $retval .= $extra['dt']->Format($_BD_CONF['format'], true);
+        break;
+    }
+    return $retval;
+}
+
+
+/**
+*   Create a month selector
+*
+*   @param  integer $month  Currently-selected month
+*   @return string      HTML for month selection
+*/
+function select_month($month)
+{
+    global $_CONF, $LANG_MONTH, $LANG_ADMIN;
+
+    if(empty($month)) {
+        $month=strftime("%m")+0;
+    }
+    $str = "<input type=hidden name=mode value='list'> " . $LANG_BD00['sel_month'] .
+        "<select name=\"filter_month\" onChange='javascript:this.form.submit();return true'><option value=\"all\">ALL";
+    for ($i = 1; $i < 13; $i++) {
+        $sel = $i == $month ? 'selected="selected"' : '';
+        $str .= "<option value=\"$i\" $sel>{$LANG_MONTH[$i]}</option>";
+    }
+    $str.='</select>';
+    return $str;
+}
+
+?>
