54b7a6c61b777619cc0242e322bac4cfc0e00896
Lee Garner <lee@leegarner.com>
2018-03-20 13:53:46 -0700

    Reorder fields as month,day

--- sql/mysql_install.php
+++ sql/mysql_install.php
@@ -2,8 +2,8 @@
 $_SQL = array(
 'birthdays' => "CREATE TABLE gl_birthdays (
   uid int(10) NOT NULL,
-  day int(2) default NULL,
   month int(2) default NULL,
+  day int(2) default NULL,
   PRIMARY KEY  (uid)
 ) TYPE=MyISAM;",
 );
