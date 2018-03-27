e5bb38deb769eea64b2d6b604984f0b6c7dcfe6f
Lee Garner <lee@leegarner.com>
2018-03-27 12:02:18 -0700

    Fix table name in installation

--- sql/mysql_install.php
+++ sql/mysql_install.php
@@ -1,6 +1,6 @@
 <?php
 $_SQL = array(
-'birthdays' => "CREATE TABLE gl_birthdays (
+'birthdays' => "CREATE TABLE {$_TABLES['birthdays']} (
   uid int(10) NOT NULL,
   month int(2) default NULL,
   day int(2) default NULL,
