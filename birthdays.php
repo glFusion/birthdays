57a56afdfd972c811d141f416d974fe0bcf61b74
Lee Garner <lee@leegarner.com>
2020-09-12 09:01:39 -0700

    Bump dev version

--- birthdays.php
+++ birthdays.php
@@ -9,7 +9,7 @@
  * @copyright   Copyright (c) 2017-2019 Lee Garner <lee@leegarner.com>
  * @copyright   Copyright (c) 2002 Mike Lynn <mike@mlynn.com>
  * @package     birthdays
- * @version     v1.0.1
+ * @version     v1.0.2
  * @license     http://opensource.org/licenses/gpl-2.0.php
  *              GNU Public License v2 or later
  * @filesource
@@ -24,7 +24,7 @@ global $_BD_CONF;
 
 $_BD_CONF['pi_name']            = 'birthdays';
 $_BD_CONF['pi_display_name']    = 'Birthdays';
-$_BD_CONF['pi_version']         = '1.0.1';
+$_BD_CONF['pi_version']         = '1.0.2';
 $_BD_CONF['gl_version']         = '1.7.8';
 $_BD_CONF['pi_url']             = 'http://www.glfusion.org';
 
